2d753df03f773696e928104ca86477d4215bdbc3
Oliver Elias <oliver.elias@outlook.com>
2018-08-10 15:56:32 +1000

    Bind port on deployment server

--- index.js
+++ index.js
@@ -48,10 +48,16 @@ app.get('/getAuth/', (req, res) => {
   res.send(req.user);
 });
 
-app.use(express.static('./client/build'));
-const path = require('path');
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '.', 'client', 'build', 'index.html'));
-});
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('./client/build'));
 
-app.listen(5000);
+  // Express will serve index.html for any
+  // unrecognized path
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '.', 'client', 'build', 'index.html'));
+  });
+}
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
