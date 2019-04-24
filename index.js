202b4dbe1c7943f80c7435bd9e8ef4c8e39b2f26
Oli <oliver.elias@outlook.com>
2019-04-24 20:57:05 +1000

    Remove database writing for authenticated users
    Writing user data to the database was unecessary and basically just
    posed a security risk. Keeping all info in session and just requesting
    login on page load is fine.

--- index.js
+++ index.js
@@ -1,60 +1,37 @@
-const express = require('express');
-const passport = require('passport');
-const TwitterStrategy = require('passport-twitter').Strategy;
-const cookieSession = require('cookie-session');
+const express = require("express");
+const passport = require("passport");
+const TwitterStrategy = require("passport-twitter").Strategy;
+const cookieSession = require("cookie-session");
 
-const config = require('./config/config.js');
-
-const mongoose = require('mongoose');
-const userSchema = new mongoose.Schema({
-  token: String,
-  tokenSecret: String,
-  twitterId: String,
-  username: String,
-  displayName: String,
-  profileImageUrl: String,
-});
-
-const User = mongoose.model('users', userSchema);
-mongoose.connect(
-  config.MONGO_URI,
-  { useNewUrlParser: true }
-); // suppress deprecation warning
+const config = require("./config/config.js");
 
 passport.use(
   new TwitterStrategy(
     {
       consumerKey: config.TWITTER_CONSUMER_KEY,
       consumerSecret: config.TWITTER_CONSUMER_SECRET,
-      callbackURL: '/auth/callback',
+      callbackURL: "/auth/callback",
       proxy: true,
     },
-    async function(token, tokenSecret, profile, cb) {
-      const existingUser = await User.findOne({ twitterId: profile.id });
-      if (existingUser) {
-        return cb(null, existingUser);
-      }
-      const newUser = await new User({
+    function(token, tokenSecret, profile, cb) {
+      const user = {
         token: token,
         tokenSecret: tokenSecret,
         twitterId: profile.id,
         username: profile.username,
         displayName: profile.displayName,
         profileImageUrl: profile._json.profile_image_url,
-      }).save();
-      return cb(null, newUser);
+      };
+      return cb(null, user);
     }
   )
 );
 
-// user.id here is the unique id of database entry
 passport.serializeUser((user, cb) => {
-  cb(null, user.id);
+  cb(null, user);
 });
 passport.deserializeUser((obj, cb) => {
-  User.findById(obj).then(user => {
-    cb(null, user);
-  });
+  cb(null, obj);
 });
 
 const app = express();
@@ -68,17 +45,17 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-require('./routes/authRoutes')(app);
-require('./routes/apiRoutes')(app);
+require("./routes/authRoutes")(app);
+require("./routes/apiRoutes")(app);
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('./client/build'));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("./client/build"));
 
   // Express will serve index.html for any
   // unrecognized path
-  const path = require('path');
-  app.get('/*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '.', 'client', 'build', 'index.html'));
+  const path = require("path");
+  app.get("/*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, ".", "client", "build", "index.html"));
   });
 }
 
